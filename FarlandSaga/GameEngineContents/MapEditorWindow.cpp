37b6f2027021581c018b7b1a95ca324c68d23bc7
Eletos <lthlovelyg@naver.com>
2022-08-19 15:41:32 +0900

    이관형 : GUI수정

--- FarlandSaga/GameEngineContents/MapEditorWindow.cpp
+++ FarlandSaga/GameEngineContents/MapEditorWindow.cpp
@@ -97,7 +97,7 @@ void MapEditorWindow::OnGUI(GameEngineLevel* _Level, float _DeltaTime)
 
         if (nullptr != Texture)
         {
-            ImGui::BeginChildFrame(reinterpret_cast<ImGuiID>("TileSelect"), { 90 * 5, 500 });
+            ImGui::BeginChildFrame(ImGui::GetID("TileSelect"), { 90 * 5, 500 });
 
             for (size_t i = 15; i < Texture->GetTextureCount(); i++)
             {
